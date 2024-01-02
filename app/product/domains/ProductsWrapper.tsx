97bdc59df88fb7e493bec0161fdd63382f2dca3b
Bernard Kllogjri <bernard.kllogjr@zooplus.com>
2024-01-02 19:28:00 +0100

    Adding prod database

--- app/product/domains/ProductsWrapper.tsx
+++ app/product/domains/ProductsWrapper.tsx
@@ -20,7 +20,7 @@ const ProductsWrapper = ({ products }: { products: Product[] }) => {
     <div className="bg-white">
       <div className="mx-auto max-w-2xl lg:max-w-7xl">
         <h2 className="sr-only">Products</h2>
-        {products && products.length && (
+        {products && !!products.length && (
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {products.map((product) => (
               <ProductCard
